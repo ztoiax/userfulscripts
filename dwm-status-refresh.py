b5a2f3b6d7361b368193cd8afe3fda9615545d41
ztoiax <liang1124486160@gmail.com>
2021-04-07 10:57:12 +0800

    change dwm-status-refresh turn to oop and add muti thread

--- dwm-status-refresh.py
+++ dwm-status-refresh.py
@@ -3,24 +3,14 @@ import psutil
 import subprocess
 import time
 import nvidia_smi
-from datetime import datetime
+import datetime
+import asyncio
+from threading import Thread
+
 
 def disk_available(p):
     return format(psutil.disk_usage(p)[2] / 1024 / 1024 / 1024, '.2f')
 
-def network(old, interface = 'enp27s0'):
-    new = psutil.net_io_counters(pernic=True)[interface]
-    sent = (new[0] - old[0]) / 1024 // 8
-    recv = (new[1] - old[1]) / 1024 // 8
-    return (sent, recv)
-
-def diskio(old):
-    new = psutil.disk_io_counters()
-    rc = new[0] - old[0]
-    wc = new[1] - old[1]
-    rb = (new[2] - old[2]) / 1024 // 8
-    wb = (new[3] - old[3]) / 1024 // 8
-    return (rc, wc, rb, wb)
 
 def Gpu():
     nvidia_smi.nvmlInit()
@@ -28,22 +18,59 @@ def Gpu():
     r = nvidia_smi.nvmlDeviceGetUtilizationRates(handle)
     return r
 
+
 def sound():
     p = subprocess.check_output('amixer get Master', shell=True)
     return p.split()[-3].decode().strip('[]')
 
-if __name__ == '__main__':
-    interface = 'enp27s0'
-    old_net = psutil.net_io_counters(pernic=True)[interface]
-    old_io = psutil.disk_io_counters()
 
+class next(object):
+    def __init__(self, func):
+        self.func = func
+        self.next = eval(self.func)
+
+    def next_to_last(self):
+        self.last = self.next
+        self.next = eval(self.func)
+
+    def net(self):
+        self.next_to_last()
+        self.sent = (self.next[0] - self.last[0]) / 1024 // 8
+        self.recv = (self.next[1] - self.last[1]) / 1024 // 8
+        return (self.sent, self.recv)
+
+    def io(self):
+        self.next_to_last()
+        self.rc = self.next[0] - self.last[0]
+        self.wc = self.next[1] - self.last[1]
+        self.rb = (self.next[2] - self.last[2]) / 1024 // 8
+        self.wb = (self.next[3] - self.last[3]) / 1024 // 8
+        return (self.rc, self.wc, self.rb, self.wb)
+
+
+def one_minute():
     while True:
-        net = network(old_net)
-        io = diskio(old_io)
+        global time_global
+        time_global = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
+        time.sleep(60)
+
+
+def one_second():
+    while True:
+        net = net_class.net()
+        io = io_class.io()
         gpu = Gpu()
         # psutil.sensors_temperatures()
 
-        arg = f' {psutil.cpu_percent(1)}%  {gpu.gpu}%  {psutil.virtual_memory()[2]}% {gpu.memory}% ﰬ {net[1]}KB/s ﰵ {net[0]}KB/s  R {io[0]} {io[2]}KB/s W {io[1]} {io[3]}KB/s {disk_available("/")}G {disk_available("/home/tz")}G  {sound()}  {datetime.now().strftime("%Y/%m/%d %H:%M")}'
+        arg = f' {psutil.cpu_percent(1)}%  {gpu.gpu}%  {psutil.virtual_memory()[2]}% {gpu.memory}% ﰬ {net[1]}KB/s ﰵ {net[0]}KB/s  R {io[0]} {io[2]}KB/s W {io[1]} {io[3]}KB/s {disk_available("/")}G {disk_available("/home/tz")}G  {sound()}  {time_global}'
 
         subprocess.call(['xsetroot', '-name', f'{arg}'])
         time.sleep(1)
+
+if __name__ == '__main__':
+    interface = 'enp27s0'
+    net_class = next('psutil.net_io_counters(pernic=True)[interface]')
+    io_class = next('psutil.disk_io_counters()')
+    one_second()
+    one_minute_thread = Thread(target=one_minute)
+    one_minute_thread.start()
