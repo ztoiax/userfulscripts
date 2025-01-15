55a0e6f32384c10c77c69dec631c4bb8c106e403
ztoiax <liang1124486160@gmail.com>
2025-01-15 14:50:51 +0800

    修改dmenu脚本的api为wayland

--- dmenu-cphistory.sh
+++ dmenu-cphistory.sh
@@ -1,2 +1,7 @@
-#!/bin/bash
-cat ~/.zsh_history | tac | awk -F ';' '{$1=""; print$0 }' | dmenu -p "copy history" -l 10 | xclip -selection clipboard
+#!/bin/sh
+
+# x11
+# cat ~/.zsh_history | tac | awk -F ';' '{$1=""; print$0 }' | dmenu -p "copy history" -l 10 | xclip -selection clipboard
+
+# wayland
+cat ~/.zsh_history | tac | awk -F ';' '{$1=""; print$0 }' | dmenu -p "copy history" -l 10 | wl-copy
--- dmenu-cpline.sh
+++ dmenu-cpline.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-$(cat ~/.zsh_history | tail -n 2 | awk -F ';' 'NR==1 {$1="";print$0 }') | dmenu -p "copy line" -l 10 | xclip -selection clipboard
+#!/bin/sh
+# X11
+# $(cat ~/.zsh_history | tail -n 2 | awk -F ';' 'NR==1 {$1="";print$0 }') | dmenu -p "copy line" -l 10 | xclip -selection clipboard
+
+# wayland
+$(cat ~/.zsh_history | tail -n 2 | awk -F ';' 'NR==1 {$1="";print$0 }') | dmenu -p "copy line" -l 10 | wl-copy
